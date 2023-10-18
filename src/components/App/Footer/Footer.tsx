e594868155dbffceb6d0a514b7eab523d98459d6
Nicolas Saumont <saumont.nicolas@gmail.com>
2023-10-18 17:10:26 +0200

    liens footer

--- src/components/App/Footer/Footer.tsx
+++ src/components/App/Footer/Footer.tsx
@@ -3,7 +3,8 @@ import './Footer.scss';
 function Footer() {
   return (
     <footer>
-      <a href="coucou">Maxime Boué, Nicolas Saumont © 2023</a>
+      <a href="coucou">Maxime Boué</a>,
+      <a href="https://www.nicolassaumont.com/">Nicolas Saumont</a> © 2023
     </footer>
   );
 }
